bb19c8184cc054612d1a34087ecec59971d3024f
Dimitar Dinkov <dimitar.dinkov@scalefocus.com>
2023-10-10 16:00:01 +0300

    Fixing classes assignment

---
+++ ClassesAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassesAssignment
+{
+    public class Program
+    {
+        public static void Main() 
+        {
+            Task1();
+            Task2();
+        }
+
+        public static void Task1()
+        {
+            var Georgi = new Student("Georgi", 987654);
+            var Dimitar = new Student("Dimitar", 157564);
+            Dimitar.TakingExam();
+            Georgi.TakingExam();
+        }
+
+        public static void Task2()
+        {
+            University uni = new University();
+            uni.ChangeYearStartedSwitch(true);
+            if (University.HasTheStudentYearStarted())
+            {
+                Console.WriteLine("Student year has started!");
+            }
+        }
+    }
+}
