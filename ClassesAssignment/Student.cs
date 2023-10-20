a9539f9badbd2637d4bccd0180e8b204823662ec
Dimitar Dinkov <dimitar.dinkov@scalefocus.com>
2023-10-20 13:10:27 +0300

    More minor changes, removed comments

--- ClassesAssignment/Student.cs
+++ ClassesAssignment/Student.cs
@@ -1,11 +1,5 @@
 ﻿namespace ClassesAssignment
 {
-    // 1. Create class named Student with following public properties:
-    // - name
-    // - fNumber
-    // and methods: takeExam()
-    // This method should implement logic for printing out the following message: "Taking exam".
-    // Note: name and fNumber are assigned in class constructor.
     public class Student
     {
         public string? name_;
